a9c26fdf9e21079358b17311cc832598c7f35779
Martin Grotz <martin.grotz@gmx.de>
2019-08-29 17:20:56 +0200

    setup: copy empty projects for workshop

---
+++ code/CSharpWorkshop.Tests/DummyTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace CSharpDemos.Tests
+{
+    public class DummyTests
+    {
+        [Fact]
+        public void Dummy()
+        {
+            var sut = new Class1();
+            sut.AddOne(1).Should().Be(2);
+        }
+    }
+}
