67c3e170cdc3aa4f5c0ea4ca83a17b735de564fa
Patrick Drechsler <patrick.drechsler@redheads.de>
2019-09-02 07:47:30 +0200

    code/c# NonEmptyStringAssertion helper

---
+++ code/CSharpDemos.Tests/NonEmptyStringTests.cs
@@ -0,0 +1,39 @@
+using FluentAssertions;
+using Xunit;
+
+namespace CSharpDemos.Tests
+{
+    public class NonEmptyStringTests
+    {
+        [Fact]
+        public void Empty_returns_None()
+        {
+            NonEmptyString.Create("").Match(() => true, x => false);
+            NonEmptyString.Create(null).Match(() => true, _ => false);
+        }
+
+        [Fact]
+        public void Valid_returns_Some()
+        {
+            // version 1
+            NonEmptyString.Create("a").Match(
+                () => false, 
+                _ => true);
+            
+            // version 2
+            NonEmptyString.Create("a").Should().BeEqualToNonEmptyString("a");
+        }
+
+        [Fact]
+        public void Valid_has_correct_content()
+        {
+            // version 1
+            NonEmptyString.Create("a").Match(
+                () => "a".Should().Be("b"), 
+                x => x.ToString().Should().Be("a"));
+
+            // version 2
+            NonEmptyString.Create("").Should().BeNone();
+        }
+    }
+}
